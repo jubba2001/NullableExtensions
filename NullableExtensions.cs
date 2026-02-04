74748caebee1dffa402f2d13dccbb7141f5d4fdd
jubba2001 <43411709+jubba2001@users.noreply.github.com>
2026-02-04 12:48:01 +0200

    Add files via upload

---
+++ NullableExtensions.cs
@@ -0,0 +1,76 @@
+﻿
+Test("", new object(), 1);
+Test(null, new object(), 1);
+Test("null", new object(), null);
+Test("null", null, 1);
+Test("null", new object(), 1);
+
+
+static void Test(string? s, object? obj, int? i)
+{
+    Console.WriteLine($"Test(s: {(s == null ? "null" : $"\"{s}\"")}, obj: {(obj == null ? "null" : obj.ToString())}, i: {(i.HasValue ? i.Value.ToString() : "null")}):");
+
+
+    Console.Write("\tif (s): ");
+    if (s)
+    {
+        Console.WriteLine("true");
+    }
+    else
+    {
+        Console.WriteLine("false");
+    }
+
+    Console.Write("\tif (obj): ");
+    if (obj)
+    {
+        Console.WriteLine("true");
+    }
+    else
+    {
+        Console.WriteLine("false");
+    }
+
+    Console.Write("\tif (i): ");
+    if (i)
+    {
+        Console.WriteLine("true");
+    }
+    else
+    {
+        Console.WriteLine("false");
+    }
+
+    Console.WriteLine("\t(!i || !obj || !s): {0}",
+        (!i || !obj || !s) ? "true"  : "false");
+
+    Console.WriteLine("\t(!!i && !!obj && !!s): {0}",
+        (!!i && !!obj && !!s) ? "true" : "false");
+
+    Console.WriteLine();
+}
+
+static class NullableExtensions
+{
+    extension(object?)
+    {
+        public static bool operator !(object? obj) => obj == null;
+        public static bool operator true(object? obj) => obj != null;
+        public static bool operator false(object? obj) => obj == null;
+    }
+    extension(string?)
+    {
+        public static bool operator !(string? s) => string.IsNullOrEmpty(s);
+        public static bool operator true(string? s) => !string.IsNullOrEmpty(s);
+        public static bool operator false(string? s) => string.IsNullOrEmpty(s);
+
+        // Operator true/false sucks! Expecting this in C# 15:
+        // public static implicit operator bool(string? s) => string.IsNullOrEmpty(s);
+    }
+    extension<T>(T?) where T : struct
+    {
+        public static bool operator !(T? t) => !t.HasValue;
+        public static bool operator true(T? t) => t.HasValue;
+        public static bool operator false(T? t) => !t.HasValue;
+    }
+}
